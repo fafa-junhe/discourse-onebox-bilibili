4004590ed39c062c99ee32f0ac4c9e29317c6a2b
fafa_junhe <842355358@qq.com>
2020-08-24 21:21:22 +0800

    finally

--- plugin.rb
+++ plugin.rb
@@ -30,16 +30,17 @@ class Onebox::Engine::BilibiliOnebox
   end
 
   def id
-    match = @url.match(/bv.*|av.*?\//i)
-    if match[0].starts_with?("bv")
-      match[0].sub("av","")
+    match = @url.match(/(bv|av)(.*)/i)
+    if match[1] == "AV" or match[1] == "av"
+      match[2]
     else
-      decode(match[0])
+      decode(match[1]+match[2])
+      
     end
   end
 
 
   def to_html
-    "<iframe id=\"player\" src=\"https://www.bilibili.com/html/player.html?aid=#{id}&page=1&as_wide=1\" scrolling=\"no\" board=\"0\" frameborder=\"no\" framespacing=\"0\" allowfullscreen=\"true\" allowtransparency=\"true\" class=\"video_player\" style=\"border: none; margin: 0 auto;\">"
+    "<iframe id=\"player\" src=\"https://www.bilibili.com/html/player.html?aid=#{id()}&page=1&as_wide=1\" scrolling=\"no\" board=\"0\" frameborder=\"no\" framespacing=\"0\" allowfullscreen=\"true\" allowtransparency=\"true\" class=\"video_player\" style=\"border: none; margin: 0 auto;\">"
   end
 end
