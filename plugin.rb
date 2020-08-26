fcd49ab3e1717b1234f59072bd46f642339273c9
fafa_junhe <842355358@qq.com>
2020-08-26 11:10:42 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,45 +2,53 @@
 
 # name: discourse-onebox-bilibili
 # about: discourse onebox for bilibili
-# version: 0.1
+# version: 0.2
 # authors: fafa_junhe
-# url: https://github.com/
+# url: https://github.com/fafa-junhe/discourse-onebox-bilibili-1
 
 
-class Onebox::Engine::BilibiliOnebox
-  include Onebox::Engine
+module Onebox
+  module Engine
+    class BiliBiliOnebox
+      include Engine
+      matches_regexp(/^https?:\/\/(www\.)?(bilibili.com)\/(video\/)(av|bv)(\w+)/i)
+      def to_html
+        placeholder_html()
+      end
 
-  REGEX = /^https?:\/\/(www\.)?(bilibili.com)\/(video\/)(av|bv)(\w+)/i
-  matches_regexp REGEX
+      def placeholder_html
+        "<iframe src=\"https://player.bilibili.com/player.html?aid=#{id()}&amp;page=1&amp;as_wide=1\" frameborder=\"0\" width=\"640\" height=\"430\" allowfullscreen=\"true\" seamless=\"seamless\" sandbox=\"allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox allow-presentation\"></iframe>"
+      end
 
-  key = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
-  $dic = {}
-  for a in 0..57
-  	$dic[key[a]]=a
-  end
-  $s=[11,10,3,8,4,6]
-  $xor=177451812
-  $add=8728348608
-  def decode(in_)
-    r=0
-    for a in 0..5
-      r = $dic[in_[$s[a]]]*58**a + r
+      private
+      # decode copied from  https://github.com/ShiSheng233/bili_BV/blob/master/biliBV/__init__.py
+      @@key = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
+      @@dic = {}
+
+      for a in 0..57
+        @@dic[@@key[a]] = a
+      end
+
+      @@s = [11, 10, 3, 8, 4, 6]
+      @@xor = 177451812
+      @@add = 8728348608
+
+      def decode(in_)
+        r = 0
+        for a in 0..5
+          r = @@dic[in_[@@s[a]]] * 58**a + r
+        end
+      (r - @@add) ^ @@xor
+      end
+
+      def id
+        match = @url.match(/(bv|av)(.*)/i)
+        if match[1] == "BV" || match[1] == "bv"
+          decode(match[1] + match[2])
+        else
+          match[2]
+        end
+      end
     end
-  return (r-$add)^$xor
-  end
-
-  def id
-    match = @url.match(/(bv|av)(.*)/i)
-    if match[1] == "AV" or match[1] == "av"
-      match[2]
-    else
-      decode(match[1]+match[2])
-      
-    end
-  end
-
-
-  def to_html
-    "<iframe id=\"player\" src=\"https://www.bilibili.com/html/player.html?aid=#{id()}&page=1&as_wide=1\" scrolling=\"no\" board=\"0\" frameborder=\"no\" framespacing=\"0\" allowfullscreen=\"true\" allowtransparency=\"true\" class=\"video_player\" style=\"border: none; margin: 0 auto;\">"
   end
 end
